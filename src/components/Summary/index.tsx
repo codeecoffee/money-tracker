7154122070c0a2c732e674da7b7b161db30795bf
inpartec.fflopes <inpartec.fflopes@terceiros.mag.com.br>
2023-02-01 18:40:08 -0300

    added transaction component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,30 @@
-import { SummaryContainer } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { SummaryCard, SummaryContainer } from './styles';
 
-export function Summary (){
-    return (
-        <SummaryContainer>
-            
-        </SummaryContainer>
-    )
+export function Summary() {
+	return (
+		<SummaryContainer>
+			<SummaryCard>
+				<header>
+					<span>In</span>
+					<ArrowCircleUp size={32} color="#00b37e" />
+				</header>
+				<strong>$5000.45</strong>
+			</SummaryCard>
+			<SummaryCard>
+				<header>
+					<span>Out</span>
+					<ArrowCircleDown size={32} color="#f75a68" />
+				</header>
+				<strong>$5000.45</strong>
+			</SummaryCard>
+			<SummaryCard variant="green">
+				<header>
+					<span>Total</span>
+					<CurrencyDollar size={32} color="#fff" />
+				</header>
+				<strong>$5000.45</strong>
+			</SummaryCard>
+		</SummaryContainer>
+	);
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,39 @@
-import { Header } from "../../components/Header";
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import {
+	PriceHighlight,
+	TransactionsTable,
+	TrasactionsContainer,
+} from './styles';
 
-export function Transactions(){
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
+export function Transactions() {
+	return (
+		<div>
+			<Header />
+			<Summary />
+			<TrasactionsContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td width="50%">Web Dev</td>
+							<td>
+								<PriceHighlight variant="income">$12000</PriceHighlight>
+							</td>
+							<td>Sold</td>
+							<td>12/26/2023</td>
+						</tr>
+						<tr>
+							<td width="50%">Food</td>
+							<td>
+								<PriceHighlight variant="outcome">- $3000</PriceHighlight>
+							</td>
+
+							<td>Sold</td>
+							<td>10/26/2023</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TrasactionsContainer>
+		</div>
+	);
 }
